368aefb557e3f3df36e9ad2df03cb9cc694f1004
Jinwoo <njw9108@instapay.kr>
2022-08-24 18:19:41 +0900

    firestore test

--- lib/presentation/home/home_view_model.dart
+++ lib/presentation/home/home_view_model.dart
@@ -7,6 +7,7 @@ import 'package:flutter_google_sheet_test/domain/model/book_data/book_data.dart'
 import 'package:flutter_google_sheet_test/domain/model/book_intro/book_intro.dart';
 import 'package:flutter_google_sheet_test/domain/use_case/sheet_use_case.dart';
 import 'package:flutter_google_sheet_test/presentation/home/home_state.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 class HomeViewModel with ChangeNotifier {
   final SheetUseCase sheetUseCase;
@@ -24,81 +25,111 @@ class HomeViewModel with ChangeNotifier {
   StreamSubscription<QuerySnapshot>? _bookDataSubscription;
 
   Future<void> loadData() async {
-//     _bookDataSubscription = FirebaseFirestore.instance
-//         .collection('temp3')
-//         .doc('timestamp')
-// //        .collection('sub')
-// //        .orderBy('timestamp', descending: true)
-//         .snapshots()
-//         .listen((snapshot) {
-//       List<BookData> temp = [];
-//       print(snapshot.docs.length);
-//       for (final document in snapshot.docs) {
-//         temp.add(
-//           BookData(
-//             booktitle: document.data()['issue'] as String,
-//             author: document.data()['mainTitle'] as String,
-//           ),
-//         );
-//       }
-//
-//       print(temp);
-//     });
+    //Firestore data Load
+    DateTime? lastSavedDate = await getFirestoreLastSaveDate();
+    List<BookIntro> bookIntro = await getFirestoreBookIntro();
+    List<Map<String, List<BookData>>> totalBookData =
+        await getFirestoreBookData();
 
-    final docRef = FirebaseFirestore.instance.collection('bookIntro');
-    final bookIntroRef = await docRef.get();
+    final List<String> firestoreIssues = [];
+    for (final data in totalBookData) {
+      firestoreIssues.add(data.keys.first);
+    }
 
+    if (lastSavedDate == null ||
+        DateTime.now().difference(lastSavedDate).inDays > 0) {
+      print('google sheet에서 읽기');
+      List<String> googleSheetIssues = [];
+      //google sheet에서 book intro 읽기
+      List<BookIntro> bookIntroList = await getBookIntroListFromGoogleSheet();
+      for (final bookIntro in bookIntroList) {
+        googleSheetIssues.add(bookIntro.issue);
+      }
+      firestoreIssues.sort();
+      googleSheetIssues.sort();
+
+      if (const DeepCollectionEquality()
+          .equals(firestoreIssues, googleSheetIssues)) {
+        //google sheet의 bookData 읽을 필요없음
+
+      } else {
+        //google sheet의 bookData 읽어서 firstore에 저장
+
+        //1.google sheet의 issue 중 firestore에 저장되어있는 issue는 빼기(중복 제거)
+        for (final firestoreIssue in firestoreIssues) {
+          googleSheetIssues.remove(firestoreIssue);
+        }
+        //2.google sheet에서 book data load(firestore에 없는것만)
+        if (googleSheetIssues.isNotEmpty) {
+          final List<Map<String, List<BookData>>> addedBookDataList = await totalBookDataLoadFromGoogleSheet(googleSheetIssues);
+
+          //3.totalBookData와 새로 읽어온 BookData 합침
+          totalBookData = List.from(totalBookData)..addAll(addedBookDataList);
+
+          //4.totalBookData를 firestore에 저장
+
+          //5.book intro를 firestore에 저장
+
+          //6.curBookData를 잘 입력하면 됨.
+        }
+      }
+    }
+  }
+
+  Future<List<Map<String, List<BookData>>>> getFirestoreBookData() async {
+    List<Map<String, List<BookData>>> totalBookData = [];
+    final bookDocRef = FirebaseFirestore.instance.collection('bookData');
+    final bookDataRef = await bookDocRef.get();
+    for (final doc in bookDataRef.docs) {
+      Iterable data = doc.data()['data'];
+      List<BookData> bookData = data.map((e) => BookData.fromJson(e)).toList();
+      totalBookData.add({doc.id: bookData});
+    }
+    return totalBookData;
+  }
+
+  Future<List<BookIntro>> getFirestoreBookIntro() async {
+    final bookIntroRef =
+        await FirebaseFirestore.instance.collection('bookIntro').get();
+    List<BookIntro> bookIntro = [];
+    bookIntro =
+        bookIntroRef.docs.map((e) => BookIntro.fromJson(e.data())).toList();
+    return bookIntro;
+  }
+
+  Future<DateTime?> getFirestoreLastSaveDate() async {
     final timestampSnapshot = await FirebaseFirestore.instance
         .collection('timestamp')
         .doc('timestamp')
         .get();
-    final time = timestampSnapshot.data();
-    Timestamp? temp = time?['timestamp'];
-    final DateTime? lastSavedDate = temp?.toDate();
-
-    List<BookIntro> bookIntro = [];
-    List<BookData> bookData = [];
-
-    bookIntro =
-        bookIntroRef.docs.map((e) => BookIntro.fromJson(e.data())).toList();
-
-    if (lastSavedDate == null ||
-        DateTime.now().difference(lastSavedDate).inDays > 0) {
-      print('엑셀에서 읽기');
-      await initAndLoadSheet(bookIntro: bookIntro, bookData: bookData);
-    }
+    final DateTime? lastSavedDate =
+        timestampSnapshot.data()?['timestamp']?.toDate();
+    return lastSavedDate;
   }
 
   void disposeData() {
     _bookDataSubscription?.cancel();
   }
 
-  Future<void> initAndLoadSheet({
-    required List<BookIntro> bookIntro,
-    required List<BookData> bookData,
-  }) async {
+  Future<List<BookIntro>> getBookIntroListFromGoogleSheet() async {
+    // _state = state.copyWith(
+    //   isLoading: true,
+    // );
+    // notifyListeners();
     await sheetUseCase.init();
-    await loadIntroDataFromGoogleSheet(
-        bookIntro: bookIntro, bookData: bookData);
-  }
-
-  Future<void> loadIntroDataFromGoogleSheet({
-    required List<BookIntro> bookIntro,
-    required List<BookData> bookData,
-  }) async {
-    _state = state.copyWith(
-      isLoading: true,
-    );
-    notifyListeners();
     final res = await sheetUseCase.getIntroAll();
-    List<String> issues = [];
-    res.forEach((element) {
-      issues.add(element.issue);
-    });
 
-    bookIntro.forEach((element) {
+    return res;
+    /*
+    * List<String> issues = [];
+
+    for (var element in res) {
+      issues.add(element.issue);
+    }
+
+    for (var element in bookIntro) {
       //print(element.issue);
-    });
+    }
 
     print('res length ${res.length}');
     await totalBookDataLoad(issues);
@@ -116,6 +147,7 @@ class HomeViewModel with ChangeNotifier {
         'timestamp': FieldValue.serverTimestamp(),
       });
     }
+
     //print(res);
     //
     // final bookData = await loadPageData(res[0].issue);
@@ -129,6 +161,7 @@ class HomeViewModel with ChangeNotifier {
     // );
 
     notifyListeners();
+    * */
   }
 
   void pageChange(int index) async {
@@ -156,35 +189,35 @@ class HomeViewModel with ChangeNotifier {
     return res;
   }
 
-  Future<void> totalBookDataLoad(List<String> issues) async {
+  Future<List<Map<String, List<BookData>>>> totalBookDataLoadFromGoogleSheet(List<String> issues) async {
     List<Map<String, List<BookData>>> totalBookData = [];
+
+    print('google sheet에서 issue 페이지의 데이터 읽어오기');
     for (final issue in issues) {
+      //issue page 초기화
       await sheetUseCase.initPage(issue);
+      //issue page에 있는 데이터 가져옴
       final res = await sheetUseCase.getBookData();
+      //totalBookData에 데이터 저장
       totalBookData.add({issue: res});
     }
-    print(totalBookData[0].keys.toList().first);
 
-    for (final bookIssue in totalBookData) {
-      String issue = bookIssue.keys.toList().first;
-      List<BookData> bookDataList = bookIssue[issue] as List<BookData>;
+    return totalBookData;
 
-      //print();
-
-      // FirebaseFirestore.instance
-      //     .collection('bookData')
-      //     .doc(issue)
-      //     .set({'data':bookDataList});
-
-      List<Map<String,dynamic>> temp = [];
-      for (final bookData in bookDataList) {
-        temp.add(bookData.toJson());
-      }
-      FirebaseFirestore.instance
-          .collection('bookData')
-          .doc(issue)
-          .set({'data':temp});
-    }
+    // print('google sheet에서 읽어온 데이터 firestore에 저장');
+    // for (final bookIssue in totalBookData) {
+    //   String issue = bookIssue.keys.toList().first;
+    //   List<BookData> bookDataList = bookIssue[issue] as List<BookData>;
+    //
+    //   List<Map<String,dynamic>> temp = [];
+    //   for (final bookData in bookDataList) {
+    //     temp.add(bookData.toJson());
+    //   }
+    //   FirebaseFirestore.instance
+    //       .collection('bookData')
+    //       .doc(issue)
+    //       .set({'data':temp});
+    // }
   }
 
   String getMainImage(int index) {
